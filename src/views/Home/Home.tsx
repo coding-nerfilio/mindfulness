b0274b369e1e0441eb036b439fd19373d7cc64ff
francisco risso <frisso2001@gmail.com>
2024-06-09 22:32:12 -0300

    Add custom scrollbar

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,17 +1,22 @@
 import {Box, Typography} from '@mui/material'
 import mockData from '../../mock/mock-data.json'
+import Scrollbar from 'react-scrollbars-custom'
 
 const Home = () => {
     return (
         <Box className="PageContainer">
-            {mockData.map((item) => (
-                <Box key={item.title} className="ArrayContainer">
-                    <Typography variant="subtitle1" fontWeight="bold">
-                        {item.title}
-                    </Typography>
-                    <Typography variant="body2">{item.description}</Typography>
-                </Box>
-            ))}
+            <Scrollbar>
+                {mockData.map((item) => (
+                    <Box key={item.title} className="ArrayContainer">
+                        <Typography variant="subtitle1" fontWeight="bold">
+                            {item.title}
+                        </Typography>
+                        <Typography variant="body2">
+                            {item.description}
+                        </Typography>
+                    </Box>
+                ))}
+            </Scrollbar>
         </Box>
     )
 }
